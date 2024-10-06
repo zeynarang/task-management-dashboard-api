5b4981ca8867d3f39e5d0c1a15e294074540d266
zeynarang <jatin.narang1095@gmail.com>
2024-10-07 03:28:52 +0530

    Return section as well from projects APIs

--- src/handler/getProject.js
+++ src/handler/getProject.js
@@ -1,24 +1,33 @@
-import express from "express";
-import pool from "../client/client.js";
 import { jwtDecode } from "jwt-decode";
+import pool from "../client/client.js";
 
 const getProjects = async (req, res) => {
   try {
+    const { projectId } = req.params;
     // Decode the JWT to get the user information
     const decodedToken = jwtDecode(req.headers.authorization);
 
     // Query to get the projects and their associated sections
     const projectsResult = await pool.query(
-      `SELECT p.*, json_agg(ps.section_id) as sections
-     FROM projects p
-     LEFT JOIN project_sections_mapping ps ON p.id = ps.project_id
-     WHERE p.user_id = $1
-     GROUP BY p.id`,
-      [decodedToken.id]
+      `SELECT 
+    p.*, 
+    COALESCE(
+        json_agg(
+            json_build_object('section_id', s.section_id, 'name', s.name)
+        ) FILTER (WHERE s.section_id IS NOT NULL), '[]'
+    ) as sections
+FROM projects p
+LEFT JOIN project_sections_mapping psm ON p.project_id = psm.project_id
+LEFT JOIN sections s ON psm.section_id = s.section_id
+WHERE p.user_id = $1 AND p.project_id = $2
+GROUP BY p.project_id 
+LIMIT 1;
+`,
+      [decodedToken.id, projectId]
     );
 
     // Return the projects and their associated sections
-    res.json(projectsResult.rows);
+    res.json(projectsResult.rows[0]);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
--- src/handler/getProjects.js
+++ src/handler/getProjects.js
@@ -1,13 +1,22 @@
-import express from "express";
-import pool from "../client/client.js";
 import { jwtDecode } from "jwt-decode";
+import pool from "../client/client.js";
 
 const getProjects = async (req, res) => {
   try {
-    const decodedToken = jwtDecode(req.headers.authorization);
+    const user = jwtDecode(req.headers.authorization);
+    console.log("user", user);
     const projectsResult = await pool.query(
-      "SELECT * FROM projects where user_id=$1",
-      [decodedToken.id]
+      `
+    SELECT 
+    p.*, 
+    COALESCE(json_agg(json_build_object('section_id', s.section_id, 'name', s.name)) FILTER (WHERE s.section_id IS NOT NULL), '[]') AS sections
+FROM projects p
+LEFT JOIN project_sections_mapping psm ON p.project_id = psm.project_id
+LEFT JOIN sections s ON psm.section_id = s.section_id
+WHERE p.user_id = $1
+GROUP BY p.project_id;
+`,
+      [user.id]
     );
     res.json(projectsResult.rows);
   } catch (err) {
