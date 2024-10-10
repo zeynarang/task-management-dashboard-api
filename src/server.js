11bc8f85526028b8a0287effee1c5144b6bf5293
zeynarang <jatin.narang1095@gmail.com>
2024-10-11 03:19:17 +0530

    Fix Routes Path

--- src/server.js
+++ src/server.js
@@ -2,15 +2,19 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import createProject from "./handler/createProject.js";
-import createTask from "./handler/createTask.js";
-import getProjects from "./handler/getProjects.js";
-import getProject from "./handler/getProject.js";
-import getTasks from "./handler/getTask.js";
-import getUserDetails from "./handler/getUser.js";
-import loginRouter from "./handler/loginUser.js";
-import registerUser from "./handler/registerUser.js";
-import createSection from "./handler/createSection.js";
+import createProject from "./handler/project/createProject.js";
+
+import createTask from "./handler/task/createTask.js";
+import getTasks from "./handler/task/getTasks.js";
+
+import getProjects from "./handler/project/getProjects.js";
+import getProject from "./handler/project/getProject.js";
+
+import createSection from "./handler/section/createSection.js";
+
+import getUserDetails from "./handler/user/getUser.js";
+import loginRouter from "./handler/user/loginUser.js";
+import registerUser from "./handler/user/registerUser.js";
 
 dotenv.config();
 
@@ -22,16 +26,27 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Use the routers
-app.post("/api/auth/signup", registerUser);
+
+// AUTH //
 app.get("/api/auth/login", loginRouter);
+app.post("/api/auth/signup", registerUser);
+
+// USERS //
 app.get("/api/user/:userId", getUserDetails);
+
+// PROJECT //
 app.get("/api/projects", getProjects);
 app.get("/api/project/:projectId", getProject);
 app.post("/api/projects", createProject);
+
+// TASKS //
 app.get("/api/tasks", getTasks);
 app.post("/api/tasks", createTask);
-app.post("/sections", createSection);
+
+// SECTIONS //
+app.post("/api/project/:projectId/sections", createSection);
 // Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
