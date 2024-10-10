2df0053b540b847cf85371a5bb0d84f7611d6b6e
zeynarang <jatin.narang1095@gmail.com>
2024-10-11 03:16:13 +0530

    Add User Auth APIs

---
+++ src/handler/user/loginUser.js
@@ -0,0 +1,46 @@
+import bcrypt from "bcrypt";
+import pool from "../../client/client.js";
+import jwt from "jsonwebtoken";
+
+const loginRouter = async (req, res) => {
+  const { email, password } = req.query;
+
+  try {
+    const userResult = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (userResult.rows.length === 0) {
+      return res.status(401).send("Invalid email or password");
+    }
+
+    const user = userResult.rows[0];
+
+    // Compare the password
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log("isMatch", isMatch);
+    if (!isMatch) {
+      return res.status(401).send("Invalid email or password");
+    }
+    // Create JWT payload
+    const payload = {
+      id: user.user_id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+    };
+
+    // Generate JWT token
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    }); // Token expires in 1 hour
+
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
+
+export default loginRouter;
---
+++ src/handler/user/registerUser.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import pool from "../../client/client.js";
+
+const registerUser = async (req, res) => {
+  const { first_name, last_name, email, password } = req.body;
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await pool.query(
+      "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
+      [first_name, last_name, email, hashedPassword]
+    );
+    res.status(201).json(newUser.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
+
+export default registerUser;
